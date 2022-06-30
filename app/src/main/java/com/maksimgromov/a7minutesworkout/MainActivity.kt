a36e729699f6fe5e30d0f85f4f3e30e3d20905ef
Maksim Gromov <mgromov@icloud.com>
2022-06-30 16:42:02 +0700

    Working commit.

--- app/src/main/java/com/maksimgromov/a7minutesworkout/MainActivity.kt
+++ app/src/main/java/com/maksimgromov/a7minutesworkout/MainActivity.kt
@@ -2,10 +2,30 @@ package com.maksimgromov.a7minutesworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
+import android.widget.Toast
+import com.maksimgromov.a7minutesworkout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private  var binding : ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        binding?.flStart?.setOnClickListener {
+            Toast.makeText(
+                this@MainActivity,
+                "Here we will",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
     }
 }
